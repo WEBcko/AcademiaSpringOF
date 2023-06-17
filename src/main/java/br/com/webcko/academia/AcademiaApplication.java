b9ce5d94494392a36d56d9e9f33f63edc3953e9b
LuizHenz <robertgito@hotmail.com>
2023-06-16 23:14:04 -0300

    Usuarios

--- src/main/java/br/com/webcko/academia/AcademiaApplication.java
+++ src/main/java/br/com/webcko/academia/AcademiaApplication.java
@@ -2,9 +2,11 @@ package br.com.webcko.academia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-public class AcademiaApplication {
+@EnableWebSecurity
+public class 									AcademiaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AcademiaApplication.class, args);
